e5400ef7b42999efd4ed64439821122e01694b7e
Qin Xiong <qin.xiong@aduneo.com>
2019-01-24 20:04:31 +1100

    Ajoute de module de log

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -47,6 +47,24 @@ class ComposerAutoloaderInited5a7b6b645d453154b0b9ecdc7f2cd3
 
         $loader->register(true);
 
+        if ($useStaticLoader) {
+            $includeFiles = Composer\Autoload\ComposerStaticInited5a7b6b645d453154b0b9ecdc7f2cd3::$files;
+        } else {
+            $includeFiles = require __DIR__ . '/autoload_files.php';
+        }
+        foreach ($includeFiles as $fileIdentifier => $file) {
+            composerRequireed5a7b6b645d453154b0b9ecdc7f2cd3($fileIdentifier, $file);
+        }
+
         return $loader;
+    }
+}
+
+function composerRequireed5a7b6b645d453154b0b9ecdc7f2cd3($fileIdentifier, $file)
+{
+    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+        require $file;
+
+        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
     }
 }
